8b0f2d1ee96af024222c51dd7f1c839b58d9cb2f
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-17 12:17:31 -0700

    Couldn't get the destroy redirect route to work, turning in

--- app/controllers/employee_tickets_controller.rb
+++ app/controllers/employee_tickets_controller.rb
@@ -1,8 +1,15 @@
 class EmployeeTicketsController < ApplicationController
 
   def destroy
-    employee_ticket = EmployeeTicket.find(params[:id])
+    employee_ticket = EmployeeTicket.find(params[:ticket])
     employee_ticket.destroy
-    redirect_to "/employees/#{employee.id}"
+    binding.pry
+    redirect_to employee_path(employee)
+  end
+  
+  private
+
+  def employee_tickets_params
+    params.permit(:employee, :ticket)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+EmployeeTicket.destroy_all
+Ticket.destroy_all
 Employee.destroy_all
 Department.destroy_all
 
@@ -16,3 +18,15 @@ Department.destroy_all
 @employee2 = Employee.create!(name: 'Roy', level: 2, department: @dept1)
 @employee3 = Employee.create!(name: 'Moss', level: 2, department: @dept1)
 @employee4 = Employee.create!(name: 'Toby', level: 2, department: @dept2)
+
+@tix1 = Ticket.create!(subject: "Broken printer", age: 3, open: true)
+@tix2 = Ticket.create!(subject: "Desk rabbit sighting", age: 2, open: false)
+@tix3 = Ticket.create!(subject: "Computer is on fire", age: 1, open: true)
+
+@et1 = EmployeeTicket.create!(employee: @employee1, ticket: @tix1)
+@et2 = EmployeeTicket.create!(employee: @employee1, ticket: @tix2)
+@et3 = EmployeeTicket.create!(employee: @employee1, ticket: @tix3)
+
+@et4 = EmployeeTicket.create!(employee: @employee2, ticket: @tix1)
+@et5 = EmployeeTicket.create!(employee: @employee2, ticket: @tix2)
+@et6 = EmployeeTicket.create!(employee: @employee2, ticket: @tix3)
